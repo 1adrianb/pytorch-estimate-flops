a567919bec48e34fffbb008d2235f1c1e154c870
Adrian Bulat <bulat.adrian@gmail.com>
2019-05-07 16:45:16 +0100

    Fixed count for pytorch 1.1

--- test/test_ops.py
+++ test/test_ops.py
@@ -7,9 +7,9 @@ from torchvision.models import resnet18
 class Tester(unittest.TestCase):
 
     def test_overall(self):
-        expected = 1826843136
+        expected = 1826818048
         input = torch.rand(1, 3, 224, 224)
         net = resnet18()
         estimated = count_ops(net, input, print_readable=False)
-
+        
         assert(expected == estimated)
